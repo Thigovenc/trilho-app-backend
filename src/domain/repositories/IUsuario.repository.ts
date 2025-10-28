85dd0b4fe05c175331d034cdbf151f28001da442
ThiagoVenceslau <thiagovenceslau1997@gmail.com>
2025-10-27 23:16:14 -0300

    refactor: remove unnecessary comments from IUsuarioRepository interface

--- src/domain/repositories/IUsuario.repository.ts
+++ src/domain/repositories/IUsuario.repository.ts
@@ -1,21 +1,6 @@
 import { Usuario } from '../entities/usuario.entity';
-
 export interface IUsuarioRepository {
-  /**
-   * Salva uma nova entidade de usuário no banco
-   * @param usuario A entidade de domínio Usuario
-   */
   save(usuario: Usuario): Promise<Usuario>;
-
-  /**
-   * Busca um usuário pelo seu e-mail
-   * @param email O e-mail do usuário
-   */
   findByEmail(email: string): Promise<Usuario | null>;
-
-  /**
-   * Busca um usuário pelo seu ID
-   * @param id O ID do usuário
-   */
   findById(id: string): Promise<Usuario | null>;
 }
