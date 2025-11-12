df80fdd72d608b739fe91f05753ac55044a642be
ThiagoVenceslau <thiagovenceslau1997@gmail.com>
2025-11-11 21:33:25 -0300

    feat: add route for marking habits as completed

--- src/routes/habito.routes.ts
+++ src/routes/habito.routes.ts
@@ -2,7 +2,11 @@ import { Router } from 'express';
 import { authMiddleware } from '../middleware/auth.middleware';
 import validate from '../middleware/validate.middleware';
 import { criarHabitoSchema } from '../validations/habito.validation';
-import { criarHabito, listarHabitos } from '../controllers/habito.controller';
+import {
+  criarHabito,
+  listarHabitos,
+  marcarComoConcluido,
+} from '../controllers/habito.controller';
 
 const router = Router();
 router.use(authMiddleware);
@@ -12,5 +16,8 @@ router.post('/', validate(criarHabitoSchema), criarHabito);
 
 // @route   GET /api/habitos
 router.get('/', listarHabitos);
+
+// @route   POST /api/habitos/:id/complete
+router.post('/:id/complete', marcarComoConcluido);
 
 export default router;
