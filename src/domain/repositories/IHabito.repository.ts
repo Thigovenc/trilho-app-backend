79017c366d26db63fee1f03e8d65526227e58545
ThiagoVenceslau <thiagovenceslau1997@gmail.com>
2025-11-11 21:32:25 -0300

    feat: implement findById and update methods in MongooseHabitoRepository with error handling

--- src/domain/repositories/IHabito.repository.ts
+++ src/domain/repositories/IHabito.repository.ts
@@ -3,4 +3,6 @@ import { Habito } from '../entities/habito.entity';
 export interface IHabitoRepository {
   save(habito: Habito): Promise<Habito>;
   findByUsuarioId(usuarioId: string): Promise<Habito[]>;
+  findById(id: string): Promise<Habito | null>;
+  update(habito: Habito): Promise<Habito>;
 }
--- src/repositories/habito.repository.ts
+++ src/repositories/habito.repository.ts
@@ -2,11 +2,12 @@ import HabitoModel, { IHabito as IHabitoModel } from '../models/habito.model';
 import { Habito } from '../domain/entities/habito.entity';
 import { IHabitoRepository } from '../domain/repositories/IHabito.repository';
 import { EnumHabitColor, EnumHabitIcon } from '../domain/enums/habito.enums';
+import mongoose from 'mongoose';
 
 const toPersistence = (habito: Habito) => {
   return {
-    _id: habito.id,
-    usuarioId: habito.usuarioId,
+    _id: new mongoose.Types.ObjectId(habito.id),
+    usuarioId: new mongoose.Types.ObjectId(habito.usuarioId),
     nome: habito.nome,
     cor: habito.cor,
     icone: habito.icone,
@@ -43,5 +44,31 @@ export class MongooseHabitoRepository implements IHabitoRepository {
     });
 
     return habitosModel.map(toDomain);
+  }
+
+  async findById(id: string): Promise<Habito | null> {
+    const habitoModel = await HabitoModel.findById(id);
+
+    if (!habitoModel) {
+      return null;
+    }
+
+    return toDomain(habitoModel);
+  }
+
+  async update(habito: Habito): Promise<Habito> {
+    const dadosPersistencia = toPersistence(habito);
+
+    const habitoModel = await HabitoModel.findByIdAndUpdate(
+      habito.id,
+      dadosPersistencia,
+      { new: true },
+    );
+
+    if (!habitoModel) {
+      throw new Error('Erro crítico: Hábito não encontrado para atualização.');
+    }
+
+    return toDomain(habitoModel);
   }
 }
